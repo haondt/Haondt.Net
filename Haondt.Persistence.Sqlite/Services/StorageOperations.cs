edfbfb0d0a036c19db2315995978cc152e2272d7
Noah <burghardtnoah@gmail.com>
2024-12-08 17:59:59 -0700

    added external storage ops for sqlite storage

---
+++ Haondt.Persistence.Sqlite/Services/StorageOperations.cs
@@ -0,0 +1,10 @@
+﻿using Haondt.Persistence.Services;
+using Microsoft.Data.Sqlite;
+
+namespace Haondt.Persistence.Sqlite.Services
+{
+    public class CustomSqliteStorageOperation : StorageOperation
+    {
+        public required Action<SqliteConnection, SqliteTransaction> Execute { get; set; }
+    }
+}
