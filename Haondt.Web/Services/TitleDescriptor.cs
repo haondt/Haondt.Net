2834d4e57d2fc8f31609c70eed42a11a893d8b09
Noah <burghardtnoah@gmail.com>
2025-01-09 05:48:09 -0700

    added head entry descriptor for title

---
+++ Haondt.Web/Services/TitleDescriptor.cs
@@ -0,0 +1,13 @@
+﻿using Haondt.Core.Extensions;
+using Haondt.Core.Models;
+
+namespace Haondt.Web.Services
+{
+    public class TitleDescriptor : IHeadEntryDescriptor
+    {
+        public required string Title { get; init; }
+        public Optional<string> Id { get; init; }
+
+        public string Render() => $"<title{Id.As(s => $" id=\"{s}\"").Or("")}>{Title}</title>";
+    }
+}
