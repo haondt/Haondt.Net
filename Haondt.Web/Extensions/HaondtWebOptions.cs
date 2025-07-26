d41ad3bbda1a4b4a62d32f77ee6ff91c7e997447
haondt <code@mail.haondt.dev>
2025-07-26 11:13:46 -0600

    made htmx/hyperscript stuff more configurable

---
+++ Haondt.Web/Extensions/HaondtWebOptions.cs
@@ -0,0 +1,10 @@
+﻿using Haondt.Core.Models;
+
+namespace Haondt.Web.Extensions
+{
+    public class HaondtWebOptions
+    {
+        public Optional<string> HtmxScriptUri { get; set; } = "https://unpkg.com/htmx.org@2.0.4";
+        public Optional<string> HyperscriptScriptUri { get; set; } = "https://unpkg.com/hyperscript.org@0.9.13";
+    }
+}
--- Haondt.Web/Extensions/ServiceCollectionExtensions.cs
+++ Haondt.Web/Extensions/ServiceCollectionExtensions.cs
@@ -9,7 +9,7 @@ namespace Haondt.Web.Extensions
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddHaondtWebServices(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddHaondtWebServices(this IServiceCollection services, IConfiguration configuration, HaondtWebOptions options)
         {
             services.AddHaondtWebCoreServices();
 
@@ -18,9 +18,31 @@ namespace Haondt.Web.Extensions
             services.AddTransient<IMemoryCache, MemoryCache>();
             services.AddScoped<IAssetHandler, AssetHandler>();
 
+            if (options.HtmxScriptUri.TryGetValue(out var htmxScriptUri))
+                services.AddScoped<IHeadEntryDescriptor>(_ => new ScriptDescriptor
+                {
+                    Uri = htmxScriptUri
+                });
+            if (options.HyperscriptScriptUri.TryGetValue(out var hyperscriptScriptUri))
+                services.AddScoped<IHeadEntryDescriptor>(_ => new ScriptDescriptor
+                {
+                    Uri = hyperscriptScriptUri
+                });
+
             services.AddScoped<RenderPageFilter>();
 
             return services;
+        }
+
+        public static IServiceCollection AddHaondtWebServices(this IServiceCollection services, IConfiguration configuration, Action<HaondtWebOptions> configureOptions)
+        {
+            var options = new HaondtWebOptions();
+            configureOptions(options);
+            return services.AddHaondtWebServices(configuration, options);
+        }
+        public static IServiceCollection AddHaondtWebServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services.AddHaondtWebServices(configuration, new HaondtWebOptions());
         }
     }
 }
