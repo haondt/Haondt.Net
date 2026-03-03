b2ba7098a808a8637346b9039f3efc4ec54e57b0
haondt <code@mail.haondt.dev>
2026-03-02 18:06:17 -0700

    extending stringconverter

---
+++ Haondt.Core.Tests/StringConverterTests.cs
@@ -0,0 +1,122 @@
+﻿using FluentAssertions;
+using Haondt.Core.Converters;
+using Haondt.Core.Models;
+
+namespace Haondt.Core.Tests
+{
+    public class StringConverterTests
+    {
+        public enum FooEnum
+        {
+            One,
+            Two,
+            Three = 99
+        }
+        [Fact]
+        public void WillParseNullables()
+        {
+            StringConverter.Parse<int?>("5").Should().Be((int?)5);
+            StringConverter.Parse<string?>("5").Should().Be((string?)"5");
+            StringConverter.Parse<int?>("").Should().Be(null);
+            StringConverter.Parse<string?>("").Should().Be("");
+            StringConverter.Parse<FooEnum?>("").Should().Be(null);
+            StringConverter.Parse<FooEnum?>("Two").Should().Be(FooEnum.Two);
+            StringConverter.Parse<FooEnum?>("Three").Should().Be(FooEnum.Three);
+            StringConverter.Parse<FooEnum?>("1").Should().Be(FooEnum.Two);
+            StringConverter.Parse<FooEnum?>("99").Should().Be(FooEnum.Three);
+        }
+
+        [Fact]
+        public void WillParseBoolean()
+        {
+            StringConverter.Parse<bool>("true").Should().Be(true);
+            StringConverter.Parse<bool>("false").Should().Be(false);
+            StringConverter.Parse<bool>("True").Should().Be(true);
+            StringConverter.Parse<bool>("False").Should().Be(false);
+            StringConverter.Parse<bool>("TRUE").Should().Be(true);
+            StringConverter.Parse<bool>("FALSE").Should().Be(false);
+        }
+
+        [Fact]
+        public void WillParseString()
+        {
+            StringConverter.Parse<string>("").Should().Be("");
+            StringConverter.Parse<string>("X").Should().Be("X");
+        }
+
+        [Fact]
+        public void WillParseChar()
+        {
+            StringConverter.Parse<char>("0").Should().Be('0');
+            StringConverter.Parse<char>("X").Should().Be('X');
+        }
+
+        [Fact]
+        public void WillParseNumericTypes()
+        {
+            StringConverter.Parse<sbyte>("3").Should().Be((sbyte)3);
+            StringConverter.Parse<sbyte>("-3").Should().Be((sbyte)-3);
+            StringConverter.Parse<byte>("3").Should().Be((byte)3);
+            StringConverter.Parse<short>("12").Should().Be((short)12);
+            StringConverter.Parse<short>("-12").Should().Be((short)-12);
+            StringConverter.Parse<ushort>("12").Should().Be((ushort)12);
+            StringConverter.Parse<int>("12").Should().Be((int)12);
+            StringConverter.Parse<int>("-12").Should().Be((int)-12);
+            StringConverter.Parse<uint>("12").Should().Be((uint)12);
+            StringConverter.Parse<long>("12").Should().Be((long)12);
+            StringConverter.Parse<long>("-12").Should().Be((long)-12);
+            StringConverter.Parse<ulong>("12").Should().Be((ulong)12);
+            StringConverter.Parse<double>("12").Should().Be((double)12);
+            StringConverter.Parse<double>("-12").Should().Be((double)-12);
+            StringConverter.Parse<double>("12.34").Should().Be((double)12.34);
+            StringConverter.Parse<float>("12").Should().Be((float)12);
+            StringConverter.Parse<float>("-12").Should().Be((float)-12);
+            StringConverter.Parse<float>("12.34").Should().Be((float)12.34);
+            StringConverter.Parse<decimal>("12").Should().Be((decimal)12);
+            StringConverter.Parse<decimal>("-12").Should().Be((decimal)-12);
+            StringConverter.Parse<decimal>("12.34").Should().Be((decimal)12.34);
+        }
+
+        [Fact]
+        public void WillParseDateTime()
+        {
+            var dateTime = new DateTime(2006, 1, 2, 15, 4, 5);
+            StringConverter.Parse<DateTime>("2006-01-02T15:04:05.000-07:00").Should().Be(dateTime);
+            StringConverter.Parse<DateTime>("2006-01-02T22:04:05.00000Z").Should().Be(dateTime);
+            StringConverter.Parse<DateTime>("2006-01-02 15:04:05").Should().Be(dateTime);
+            StringConverter.Parse<DateTime>("2006-01-02").Should().Be(dateTime.Date);
+        }
+
+        [Fact]
+        public void WillParseAbsoluteDateTime()
+        {
+            var dateTime = AbsoluteDateTime.Create(new DateTime(2006, 1, 2, 15, 4, 5));
+            var date = AbsoluteDateTime.Create(new DateTime(2006, 1, 2));
+            StringConverter.Parse<AbsoluteDateTime>("1136239445").Should().Be(dateTime);
+            StringConverter.Parse<AbsoluteDateTime>("2006-01-02T15:04:05.000-07:00").Should().Be(dateTime);
+            StringConverter.Parse<AbsoluteDateTime>("2006-01-02T22:04:05.00000Z").Should().Be(dateTime);
+            StringConverter.Parse<AbsoluteDateTime>("2006-01-02 15:04:05").Should().Be(dateTime);
+            StringConverter.Parse<AbsoluteDateTime>("2006-01-02").Should().Be(date);
+        }
+
+        [Fact]
+        public void WillParseGuid()
+        {
+            var guid = Guid.Parse("e3100b42-883f-4fb4-86c6-c051e8d95743");
+            StringConverter.Parse<Guid>("e3100b42-883f-4fb4-86c6-c051e8d95743").Should().Be(guid);
+            StringConverter.Parse<Guid>("E3100B42-883F-4FB4-86C6-C051E8D95743").Should().Be(guid);
+        }
+
+
+        [Fact]
+        public void WillParseEnum()
+        {
+            StringConverter.Parse<FooEnum>("One").Should().Be(FooEnum.One);
+            StringConverter.Parse<FooEnum>("Two").Should().Be(FooEnum.Two);
+            StringConverter.Parse<FooEnum>("Three").Should().Be(FooEnum.Three);
+            StringConverter.Parse<FooEnum>("0").Should().Be(FooEnum.One);
+            StringConverter.Parse<FooEnum>("1").Should().Be(FooEnum.Two);
+            StringConverter.Parse<FooEnum>("99").Should().Be(FooEnum.Three);
+        }
+    }
+}
--- Haondt.Core/Converters/StringConverter.cs
+++ Haondt.Core/Converters/StringConverter.cs
@@ -1,42 +1,79 @@
-﻿namespace Haondt.Core.Converters
+﻿using Haondt.Core.Models;
+
+namespace Haondt.Core.Converters
 {
     public static class StringConverter
     {
         private delegate bool ParseMethod<TResult>(string value, out TResult result);
+        private static InvalidCastException NewInvalidCastException<T>(string value) =>
+            new InvalidCastException($"Cannot convert '{value}' to {typeof(T).FullName}");
+        private static T Cast<T, TParsed>(TParsed parsed, string value)
+        {
+            if (parsed is T casted) return casted;
+            throw NewInvalidCastException<T>(value);
+        }
+        private static T Convert<T, TIntermediate>(ParseMethod<TIntermediate> tryParse, string value)
+        {
+            if (!tryParse(value, out var parsed))
+                throw NewInvalidCastException<T>(value);
+            return Cast<T, TIntermediate>(parsed, value);
+        }
+        private static T ParseAbsoluteDateTime<T>(string value)
+        {
+            if (long.TryParse(value, out var ticks))
+                return Cast<T, AbsoluteDateTime>(AbsoluteDateTime.Create(ticks), value);
+            else if (DateTime.TryParse(value, out var dt))
+                return Cast<T, AbsoluteDateTime>(AbsoluteDateTime.Create(dt), value);
+            throw NewInvalidCastException<T>(value);
+        }
+        private static T FallBackTryParse<T>(Type targetType, string value)
+        {
+            if (targetType == typeof(Guid))
+                return Convert<T, Guid>(Guid.TryParse, value);
+            else if (targetType == typeof(AbsoluteDateTime))
+                return ParseAbsoluteDateTime<T>(value);
+
+            throw NewInvalidCastException<T>(value);
+        }
+
+        private static T ParseEnum<T>(Type targetType, string value)
+        {
+            if (Enum.TryParse(targetType, value, ignoreCase: true, out var enumValue))
+                return Cast<T, object>(enumValue, value);
+            throw NewInvalidCastException<T>(value);
+        }
         public static T Parse<T>(string value)
         {
-            var targetType = Nullable.GetUnderlyingType(typeof(T)) ?? typeof(T);
-
-            T TryParse<TParser>(ParseMethod<TParser> parseMethod)
+            var targetType = typeof(T);
+            var underlying = Nullable.GetUnderlyingType(typeof(T));
+            if (underlying != null)
             {
-                if (!parseMethod(value, out TParser parsedValue))
-                    throw new InvalidCastException($"Cannot convert {value} to {typeof(T).FullName}");
-                if (parsedValue is not T castedValue)
-                    throw new InvalidCastException($"Cannot convert {value} to {typeof(T).FullName}");
-                return castedValue;
+                if (value == string.Empty)
+                    return default!;
+                targetType = underlying;
             }
 
-            T FallBackTryParse()
-            {
-                if (targetType == typeof(Guid))
-                    return TryParse<Guid>(Guid.TryParse);
-                throw new InvalidCastException($"Cannot convert {value} to {typeof(T).FullName}");
-            }
+            if (targetType.IsEnum)
+                return ParseEnum<T>(targetType, value);
 
             return Type.GetTypeCode(targetType) switch
             {
-                TypeCode.Boolean => TryParse<bool>(bool.TryParse),
+                TypeCode.SByte => Convert<T, sbyte>(sbyte.TryParse, value),
+                TypeCode.Byte => Convert<T, byte>(byte.TryParse, value),
+                TypeCode.Boolean => Convert<T, bool>(bool.TryParse, value),
+                TypeCode.Char => Convert<T, char>(char.TryParse, value),
                 TypeCode.String => (T)(object)value!,
-                TypeCode.Int16 => TryParse<int>(int.TryParse),
-                TypeCode.Int32 => TryParse<int>(int.TryParse),
-                TypeCode.Int64 => TryParse<int>(int.TryParse),
-                TypeCode.UInt16 => TryParse<int>(int.TryParse),
-                TypeCode.UInt32 => TryParse<int>(int.TryParse),
-                TypeCode.UInt64 => TryParse<int>(int.TryParse),
-                TypeCode.Double => TryParse<double>(double.TryParse),
-                TypeCode.Decimal => TryParse<decimal>(decimal.TryParse),
-                TypeCode.DateTime => TryParse<DateTime>(DateTime.TryParse),
-                _ => FallBackTryParse()
+                TypeCode.Int16 => Convert<T, short>(short.TryParse, value),
+                TypeCode.Int32 => Convert<T, int>(int.TryParse, value),
+                TypeCode.Int64 => Convert<T, long>(long.TryParse, value),
+                TypeCode.UInt16 => Convert<T, ushort>(ushort.TryParse, value),
+                TypeCode.UInt32 => Convert<T, uint>(uint.TryParse, value),
+                TypeCode.UInt64 => Convert<T, ulong>(ulong.TryParse, value),
+                TypeCode.Double => Convert<T, double>(double.TryParse, value),
+                TypeCode.Single => Convert<T, float>(float.TryParse, value),
+                TypeCode.Decimal => Convert<T, decimal>(decimal.TryParse, value),
+                TypeCode.DateTime => Convert<T, DateTime>(DateTime.TryParse, value),
+                _ => FallBackTryParse<T>(targetType, value)
             };
 
         }
