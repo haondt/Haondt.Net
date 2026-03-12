1dae6cfdb6006153f4a7a96baf931a8a53986134
haondt <code@mail.haondt.dev>
2026-03-11 19:10:29 -0600

    adding simplegenericoptionaljsonconverter

---
+++ Haondt.Json.Tests/SimpleGenericOptionalJsonConverterTests.cs
@@ -0,0 +1,117 @@
+﻿using FluentAssertions;
+using Haondt.Core.Models;
+using Haondt.Json.Converters;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Reflection;
+
+namespace Haondt.Json.Tests
+{
+    public class SimpleGenericOptionalData
+    {
+        public required List<Optional<List<string>>> Strings { get; set; }
+    }
+
+    public class SimpleGenericOptionalJsonConverterTests
+    {
+        private Optional<List<string>> _optional;
+        private Optional<List<string>> _optional2;
+        private Optional<int> _optional3;
+        private Optional<int> _optional4;
+        private string _json;
+        private string _json2;
+        private string _json3;
+        private string _json4;
+        private JsonSerializerSettings _serializerSettings;
+
+        public SimpleGenericOptionalJsonConverterTests()
+        {
+            _optional = new Optional<List<string>>(["1", "2"]);
+            _optional2 = new Optional<List<string>>();
+            _optional3 = new Optional<int>(77);
+            _optional4 = new Optional<int>();
+
+            var testDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
+            var filePath = Path.Combine(testDir, "optional1.json");
+            _json = @"[""1"",""2""]";
+            _json2 = @"null";
+            _json3 = @"77";
+            _json4 = @"null";
+            _serializerSettings = new();
+            _serializerSettings.Converters.Add(new SimpleGenericOptionalJsonConverter());
+        }
+
+        [Fact]
+        public void WillRespectContractResolver()
+        {
+            var serializerSettings = new JsonSerializerSettings();
+            serializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            serializerSettings.Converters.Add(new SimpleGenericOptionalJsonConverter());
+            var serialized = JsonConvert.SerializeObject(_optional, serializerSettings);
+            var deserialized = JsonConvert.DeserializeObject<Optional<List<string>>>(serialized, serializerSettings);
+            deserialized.Value.Should().BeEquivalentTo(_optional.Value);
+        }
+
+        [Fact]
+        public void WillRespectNullValueHandling()
+        {
+            var serializerSettings = new JsonSerializerSettings();
+            serializerSettings.NullValueHandling = NullValueHandling.Ignore;
+            serializerSettings.Converters.Add(new SimpleGenericOptionalJsonConverter());
+            var serialized = JsonConvert.SerializeObject(_optional4, serializerSettings);
+            serialized.Should().BeEquivalentTo(_json4);
+            var deserialized = JsonConvert.DeserializeObject<Optional<int>>(_json4, serializerSettings);
+            deserialized.HasValue.Should().Be(_optional4.HasValue);
+        }
+
+
+        [Fact]
+        public void WillSerializeOptional()
+        {
+            var serialized = JsonConvert.SerializeObject(_optional, _serializerSettings);
+            serialized.Should().Be(_json);
+        }
+
+        [Fact]
+        public void WillSerializePrimitiveOptional()
+        {
+            var serialized = JsonConvert.SerializeObject(_optional3, _serializerSettings);
+            serialized.Should().Be(_json3);
+        }
+
+        [Fact]
+        public void WillSerializeEmptyOptional()
+        {
+            var serialized = JsonConvert.SerializeObject(_optional2, _serializerSettings);
+            serialized.Should().Be(_json2);
+        }
+
+        [Fact]
+        public void WillDeserializeOptional()
+        {
+            var serialized = JsonConvert.DeserializeObject<Optional<List<string>>>(_json, _serializerSettings);
+            serialized.Value.Should().BeEquivalentTo(_optional.Value);
+        }
+
+        [Fact]
+        public void WillDeserializeEmptyOptional()
+        {
+            var serialized = JsonConvert.DeserializeObject<Optional<List<string>>>(_json2, _serializerSettings);
+            serialized.HasValue.Should().BeFalse();
+        }
+
+        [Fact]
+        public void WillDeserializeEmptyNestedOptional()
+        {
+            var serialized = JsonConvert.DeserializeObject<Optional<Optional<int>>>(_json2, _serializerSettings);
+            serialized.HasValue.Should().BeFalse();
+        }
+
+        [Fact]
+        public void WillDeserializeNestedOptional()
+        {
+            var serialized = JsonConvert.DeserializeObject<Optional<Optional<int>>>(_json3, _serializerSettings);
+            serialized.Value.Value.Should().Be(_optional3.Value);
+        }
+    }
+}
---
+++ Haondt.Json/Converters/SimpleGenericOptionalJsonConverter.cs
@@ -0,0 +1,72 @@
+﻿using Haondt.Core.Models;
+using Newtonsoft.Json;
+
+namespace Haondt.Json.Converters
+{
+    /// <summary>
+    /// A JSON converter for <see cref="Optional{T}"/> that serializes the inner value directly,
+    /// without a wrapper object. An empty <see cref="Optional{T}"/> serializes as <c>null</c>,
+    /// and a valued <see cref="Optional{T}"/> serializes as the inner value itself.
+    /// <remarks>
+    /// This converter does not support <see cref="Nullable{T}"/> of <see cref="Optional{T}"/>.
+    /// If you need to distinguish between a null <see cref="Optional{T}"/>? and an empty
+    /// <see cref="Optional{T}"/>, use <see cref="GenericOptionalJsonConverter"/> instead.
+    /// </remarks>
+    /// </summary>
+    public class SimpleGenericOptionalJsonConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType) =>
+            objectType.IsGenericType &&
+            objectType.GetGenericTypeDefinition() == typeof(Optional<>);
+
+        public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
+        {
+            var valueType = objectType.GetGenericArguments()[0];
+
+            if (reader.TokenType == JsonToken.Null)
+            {
+                var empty = objectType.GetConstructor([])!;
+                return empty.Invoke([]);
+            }
+
+            try
+            {
+                var value = serializer.Deserialize(reader, valueType);
+                var constructor = objectType.GetConstructor([valueType])!;
+                return constructor.Invoke([value]);
+            }
+            catch (Exception ex) when (ex is not JsonSerializationException)
+            {
+                throw new JsonSerializationException($"Failed to deserialize optional {objectType}", ex);
+            }
+        }
+
+        public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+        {
+            if (value == null)
+            {
+                writer.WriteNull();
+                return;
+            }
+
+            var optionalType = value.GetType();
+            var hasValueProperty = optionalType.GetProperty(nameof(Optional<>.HasValue))!;
+            var valueProperty = optionalType.GetProperty(nameof(Optional<>.Value))!;
+
+            if (!(bool)hasValueProperty.GetValue(value)!)
+            {
+                writer.WriteNull();
+                return;
+            }
+
+            try
+            {
+                serializer.Serialize(writer, valueProperty.GetValue(value));
+            }
+            catch (Exception ex) when (ex is not JsonSerializationException)
+            {
+                throw new JsonSerializationException($"Failed to serialize optional", ex);
+            }
+        }
+    }
+}
